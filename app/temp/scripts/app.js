b29a676dc32de1c1eadc1e36048151a9f204ba47
luuaAr26 <raul_cosmin_26@yahoo.com>
2017-08-12 21:49:39 +0300

    Create header and hero section for large screens

--- app/temp/scripts/app.js
+++ app/temp/scripts/app.js
@@ -3,9 +3,7 @@ webpackJsonp([1],[
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-
-
-console.log('works');
+eval("\n\nconsole.log('works');//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9hcHAvYXNzZXRzL3NjcmlwdHMvYXBwLmpzPzg4ZDMiXSwibmFtZXMiOlsiY29uc29sZSIsImxvZyJdLCJtYXBwaW5ncyI6Ijs7QUFBQUEsUUFBUUMsR0FBUixDQUFZLE9BQVoiLCJmaWxlIjoiMC5qcyIsInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKCd3b3JrcycpO1xuXG5cbi8vIFdFQlBBQ0sgRk9PVEVSIC8vXG4vLyAuL2FwcC9hc3NldHMvc2NyaXB0cy9hcHAuanMiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///0\n");
 
 /***/ })
 ],[0]);
